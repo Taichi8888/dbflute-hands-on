5032af4f265b6422c9bbf45153e8ff51ef7f094c
Taichi8888 <taichi.hasegawa@bizreach.co.jp>
2025-10-08 20:38:44 +0900

    s2：テストgreenまで。次回「はじめてのConditionBean」から

---
+++ src/test/java/org/docksidestage/handson/exercise/HandsOn02Test.java
@@ -0,0 +1,24 @@
+package org.docksidestage.handson.exercise;
+
+import javax.annotation.Resource;
+
+import org.docksidestage.handson.dbflute.exbhv.MemberBhv;
+import org.docksidestage.handson.unit.UnitContainerTestCase;
+
+public class HandsOn02Test extends UnitContainerTestCase{
+
+    @Resource
+    private MemberBhv memberBhv;
+
+    public void test_existsTestData() throws Exception {
+        // ## Arrange ##
+        
+        // ## Act ##
+        long count = memberBhv.selectCount(cb -> {});
+    
+        // ## Assert ##
+        assertTrue(count > 0);
+    }
+
+
+}
