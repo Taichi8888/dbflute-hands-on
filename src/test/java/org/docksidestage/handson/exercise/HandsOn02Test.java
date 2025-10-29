14ef688a5017a127c6bcabdfd08bd119b36fc52a
jflute <dbflute@gmail.com>
2025-10-29 19:32:29 +0900

    1on1ふぉろー

--- src/test/java/org/docksidestage/handson/exercise/HandsOn02Test.java
+++ src/test/java/org/docksidestage/handson/exercise/HandsOn02Test.java
@@ -5,25 +5,38 @@ import javax.annotation.Resource;
 import org.docksidestage.handson.dbflute.exbhv.MemberBhv;
 import org.docksidestage.handson.unit.UnitContainerTestCase;
 
-// TODO jflute 次回1on1, section2からのふぉろー (2025/10/14)
+// done jflute 次回1on1, section2からのふぉろー (2025/10/14)
+// #1on1: マスターデータとテストデータの違い (2025/10/29)
+// (会員マスター話)
+// #1on1: PK制約とは？ (PrimaryKey制約、主キー制約)
+// #1on1: UQ制約とは？ (Unique制約)
+// #1on1: FK制約とは？ (ForeignKey制約、外部キー制約)
+// #1on1: NotNull制約とは？ (insert/update時にnullが登録できない)
+// SchemaHTMLも一緒にみながら
 
 // #1on1: section1の振り返り
 // o 置くだけのMySQLの話
 // o Maven/Gradle: Mavenはxmlでレール敷いてる(安定)、Gradleはscriptで自由度が高い
 // o 特徴の違いで覚えておくといい
 // o Gradle (でビルドするとき) も、Mavenサーバーを使っている e.g. mavenCentral()
-// TODO done hase [読み物課題] 応援してる "A" にもデメリットはあるよ by jflute (2025/10/14)
+// done hase [読み物課題] 応援してる "A" にもデメリットはあるよ by jflute (2025/10/14)
 // https://jflute.hatenadiary.jp/entry/20181008/yourademerit
+// #1on1: なぜコードの体裁にこだわるのか？フラッシュ記憶しやすいように。フラッシュ記憶できると...↓
+// TODO hase [読み物課題] 別にパソコンがなくてもプログラミングはできるよ by jflute (2025/10/29)
+// https://jflute.hatenadiary.jp/entry/20170923/nopcpg
 
 // TODO jflute いつか1on1で、IntelliJの.ideaのお話 (2025/10/14)
 // https://dbflute.seasar.org/ja/tutorial/handson/section01.html#ideashared
 
-// TODO done hase DBFluteクライアントのlogのログファイルをgitignoreにお願いします by jflute (2025/10/14)
+// done hase DBFluteクライアントのlogのログファイルをgitignoreにお願いします by jflute (2025/10/14)
 // 引用:
 // ハンズオンをどこかの Git で管理する場合は、logファイルを .gitignore に登録しておきましょう。
 // (logディレクトリに.gitignoreファイルを作成し、*.logと指定しておくでOKです)
+// TODO hase 既存のコミット済みのものはそのままなので削除コミットしましょう by jflute (2025/10/29)
+// (.gitignoreのニュアンス、今後コミットするときにignoreにするって感じ)
+// #1on1: .gitignore戦略の話 (設定ファイルを綺麗に、階層をうまく使う)
 
-// TODO done hase javadocお願い by jflute (2025/10/14)
+// done hase javadocお願い by jflute (2025/10/14)
 
 /**
  * セクション2のテストクラス
@@ -47,7 +60,7 @@ public class HandsOn02Test extends UnitContainerTestCase{
         // ## Arrange ##
         
         // ## Act ##
-        // TODO done hase 戻り値はlongじゃなくて int  by jflute (2025/10/14)
+        // done hase 戻り値はlongじゃなくて int  by jflute (2025/10/14)
         // (IntelliJの補完で、戻り値を導出するようにしましょう: .var )
         int count = memberBhv.selectCount(cb -> {});
     
