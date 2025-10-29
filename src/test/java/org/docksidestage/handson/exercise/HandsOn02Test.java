a77c2388ebd6eaa069d81b285a1371eb85d39c4b
Taichi8888 <taichi.hasegawa@bizreach.co.jp>
2025-10-29 18:21:39 +0900

    s2：TODO対応。（今週は進められませんでしたすみません！！）

--- src/test/java/org/docksidestage/handson/exercise/HandsOn02Test.java
+++ src/test/java/org/docksidestage/handson/exercise/HandsOn02Test.java
@@ -12,18 +12,30 @@ import org.docksidestage.handson.unit.UnitContainerTestCase;
 // o Maven/Gradle: Mavenはxmlでレール敷いてる(安定)、Gradleはscriptで自由度が高い
 // o 特徴の違いで覚えておくといい
 // o Gradle (でビルドするとき) も、Mavenサーバーを使っている e.g. mavenCentral()
-// TODO hase [読み物課題] 応援してる "A" にもデメリットはあるよ by jflute (2025/10/14)
+// TODO done hase [読み物課題] 応援してる "A" にもデメリットはあるよ by jflute (2025/10/14)
 // https://jflute.hatenadiary.jp/entry/20181008/yourademerit
 
 // TODO jflute いつか1on1で、IntelliJの.ideaのお話 (2025/10/14)
 // https://dbflute.seasar.org/ja/tutorial/handson/section01.html#ideashared
 
-// TODO hase DBFluteクライアントのlogのログファイルをgitignoreにお願いします by jflute (2025/10/14)
+// TODO done hase DBFluteクライアントのlogのログファイルをgitignoreにお願いします by jflute (2025/10/14)
 // 引用:
 // ハンズオンをどこかの Git で管理する場合は、logファイルを .gitignore に登録しておきましょう。
 // (logディレクトリに.gitignoreファイルを作成し、*.logと指定しておくでOKです)
 
-// TODO hase javadocお願い by jflute (2025/10/14)
+// TODO done hase javadocお願い by jflute (2025/10/14)
+
+/**
+ * セクション2のテストクラス
+ * <ul>
+ *     <li>テストデータ登録の確認</li>
+ *     <li>会員名称がSで始まる会員を検索</li>
+ *     <li>会員IDが1の会員を検索</li>
+ *     <li>生年月日がない会員を検索</li>
+ * </ul>
+ *
+ * @author hase
+ */
 public class HandsOn02Test extends UnitContainerTestCase{
 
     @Resource
@@ -35,13 +47,19 @@ public class HandsOn02Test extends UnitContainerTestCase{
         // ## Arrange ##
         
         // ## Act ##
-        // TODO hase 戻り値はlongじゃなくて int  by jflute (2025/10/14)
+        // TODO done hase 戻り値はlongじゃなくて int  by jflute (2025/10/14)
         // (IntelliJの補完で、戻り値を導出するようにしましょう: .var )
-        long count = memberBhv.selectCount(cb -> {});
+        int count = memberBhv.selectCount(cb -> {});
     
         // ## Assert ##
         assertTrue(count > 0);
     }
+    public void test_candidateNameStartsWithS() throws Exception {
+        // ## Arrange ##
+        
+        
+        // ## Act ##
 
-
+        // ## Assert ##
+    }
 }
