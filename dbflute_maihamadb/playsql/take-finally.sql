4b51080119a704aa765bb855e913e3d3c7385124
Taichi8888 <taichi.hasegawa@bizreach.co.jp>
2026-06-19 07:13:54 +0900

    take-finally落とすデータ（3つだが落ちるのは最初の1つ）

--- dbflute_maihamadb/playsql/take-finally.sql
+++ dbflute_maihamadb/playsql/take-finally.sql
@@ -13,3 +13,33 @@ where exists (select subadr.MEMBER_ADDRESS_ID
                 and subadr.VALID_BEGIN_DATE < adr.VALID_END_DATE
 )
 ;
+
+-- #df:assertListZero#
+-- /- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+-- Provisional member should not have formalized datetime.
+-- - - - - - - - - - -/
+select member.MEMBER_ID, member.MEMBER_NAME, member.MEMBER_STATUS_CODE, member.FORMALIZED_DATETIME
+from MEMBER member
+where member.MEMBER_STATUS_CODE = 'PRV'
+  and member.FORMALIZED_DATETIME is not null
+;
+
+-- #df:assertListZero#
+-- /- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+-- Member should not be born in the future.
+-- - - - - - - - - - -/
+select member.MEMBER_ID, member.MEMBER_NAME, member.BIRTHDATE
+from MEMBER member
+where member.BIRTHDATE > current_date
+;
+
+-- #df:assertListZero#
+-- /- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+-- Withdrawal member should have withdrawal info.
+-- - - - - - - - - - -/
+select member.MEMBER_ID, member.MEMBER_NAME
+from MEMBER member
+  left join MEMBER_WITHDRAWAL wdl on wdl.MEMBER_ID = member.MEMBER_ID
+where member.MEMBER_STATUS_CODE = 'WDL'
+  and wdl.MEMBER_ID is null
+;
