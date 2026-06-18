9d5cdfaee0805049ebf19ebe2dcc34883796aca7
Taichi8888 <taichi.hasegawa@bizreach.co.jp>
2026-06-19 07:01:28 +0900

    Member addresses should be only one at any time.の実験

--- dbflute_maihamadb/playsql/take-finally.sql
+++ dbflute_maihamadb/playsql/take-finally.sql
@@ -1,1 +1,15 @@
- 
+-- #df:assertListZero#
+-- /- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+-- Member addresses should be only one at any time.
+-- - - - - - - - - - -/
+select adr.MEMBER_ADDRESS_ID, adr.MEMBER_ID
+     , adr.VALID_BEGIN_DATE, adr.VALID_END_DATE
+     , adr.ADDRESS
+from MEMBER_ADDRESS adr
+where exists (select subadr.MEMBER_ADDRESS_ID
+              from MEMBER_ADDRESS subadr
+              where subadr.MEMBER_ID = adr.MEMBER_ID
+                and subadr.VALID_BEGIN_DATE > adr.VALID_BEGIN_DATE
+                and subadr.VALID_BEGIN_DATE < adr.VALID_END_DATE
+)
+;
