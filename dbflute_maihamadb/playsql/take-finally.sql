1dad4cec2d1a10522371460105b99a3d555157ed
Taichi8888 <taichi.hasegawa@bizreach.co.jp>
2026-06-19 07:22:24 +0900

    take-finally CBスタイル修正、おまけ修正。s5完

--- dbflute_maihamadb/playsql/take-finally.sql
+++ dbflute_maihamadb/playsql/take-finally.sql
@@ -5,41 +5,41 @@
 select adr.MEMBER_ADDRESS_ID, adr.MEMBER_ID
      , adr.VALID_BEGIN_DATE, adr.VALID_END_DATE
      , adr.ADDRESS
-from MEMBER_ADDRESS adr
-where exists (select subadr.MEMBER_ADDRESS_ID
-              from MEMBER_ADDRESS subadr
-              where subadr.MEMBER_ID = adr.MEMBER_ID
-                and subadr.VALID_BEGIN_DATE > adr.VALID_BEGIN_DATE
-                and subadr.VALID_BEGIN_DATE < adr.VALID_END_DATE
-)
+  from MEMBER_ADDRESS adr
+ where exists (select subadr.MEMBER_ADDRESS_ID
+                 from MEMBER_ADDRESS subadr
+                where subadr.MEMBER_ID = adr.MEMBER_ID
+                  and subadr.VALID_BEGIN_DATE > adr.VALID_BEGIN_DATE
+                  and subadr.VALID_BEGIN_DATE <= adr.VALID_END_DATE
+               )
 ;
 
 -- #df:assertListZero#
 -- /- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 -- Provisional member should not have formalized datetime.
 -- - - - - - - - - - -/
-select member.MEMBER_ID, member.MEMBER_NAME, member.MEMBER_STATUS_CODE, member.FORMALIZED_DATETIME
-from MEMBER member
-where member.MEMBER_STATUS_CODE = 'PRV'
-  and member.FORMALIZED_DATETIME is not null
+select mb.MEMBER_ID, mb.MEMBER_NAME, mb.MEMBER_STATUS_CODE, mb.FORMALIZED_DATETIME
+  from MEMBER mb
+ where mb.MEMBER_STATUS_CODE = 'PRV'
+   and mb.FORMALIZED_DATETIME is not null
 ;
 
 -- #df:assertListZero#
 -- /- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 -- Member should not be born in the future.
 -- - - - - - - - - - -/
-select member.MEMBER_ID, member.MEMBER_NAME, member.BIRTHDATE
-from MEMBER member
-where member.BIRTHDATE > current_date
+select mb.MEMBER_ID, mb.MEMBER_NAME, mb.BIRTHDATE
+  from MEMBER mb
+ where mb.BIRTHDATE > current_date
 ;
 
 -- #df:assertListZero#
 -- /- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 -- Withdrawal member should have withdrawal info.
 -- - - - - - - - - - -/
-select member.MEMBER_ID, member.MEMBER_NAME
-from MEMBER member
-  left join MEMBER_WITHDRAWAL wdl on wdl.MEMBER_ID = member.MEMBER_ID
-where member.MEMBER_STATUS_CODE = 'WDL'
-  and wdl.MEMBER_ID is null
+select mb.MEMBER_ID, mb.MEMBER_NAME
+  from MEMBER mb
+    left join MEMBER_WITHDRAWAL wdl on wdl.MEMBER_ID = mb.MEMBER_ID
+ where mb.MEMBER_STATUS_CODE = 'WDL'
+   and wdl.MEMBER_ID is null
 ;
