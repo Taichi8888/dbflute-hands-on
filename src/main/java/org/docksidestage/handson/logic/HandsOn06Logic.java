d777cdf09165980ffee57bed6b1ca2a7b3b4a3c0
jflute <jflute@localhost>
2026-06-30 16:57:00 +0900

    1on1ふぉろー

--- src/main/java/org/docksidestage/handson/logic/HandsOn06Logic.java
+++ src/main/java/org/docksidestage/handson/logic/HandsOn06Logic.java
@@ -8,6 +8,13 @@ import org.docksidestage.handson.dbflute.exentity.Member;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+// #1on1: ログの設定、一度は目にしておこう (2026/06/30)
+// #1on1: 和名の設定 (2026/06/30)
+// #1on1: 大文字小文字の設定 (2026/06/30)
+// #1on1: 空文字入れない設定 (2026/06/30)
+// #1on1: DBFluteアップグレード、DBFlute Client, DBFlute Engine, DBFlute Runtime (2026/06/30)
+// #1on1: decommentのお話 (2026/06/30)
+// #1on1: SchemaPolicyCheckのお話、DB設計の統一性の担保 (2026/06/30)
 public class HandsOn06Logic {
 
     private static final Logger logger = LoggerFactory.getLogger(HandsOn06Logic.class);
